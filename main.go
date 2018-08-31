ae26963022b4147cb82db0c0aee95dc13eb3cf20
Patrick McNamara <hello@patrickmcnamara.xyz>
2018-08-31 19:54:17 +0100

    Add comments and format code better

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Down checks if your internet connection is down.
 package main
 
 import (
@@ -6,13 +7,20 @@ import (
 )
 
 const (
-	up   = "\033[1;38;5;2mUP\033[0m"
+	// up is a string for printing "UP" in terminal with green text.
+	up = "\033[1;38;5;2mUP\033[0m"
+	// down is a string for printing "DOWN" in terminal with a red background.
 	down = "\033[1;48;5;1mDOWN\033[0m"
 )
 
-func main() {
+// downCheck checks if the internet connection is down.
+func downCheck() bool {
 	_, err := http.Get("https://google.com/")
-	if err != nil {
+	return err != nil
+}
+
+func main() {
+	if downCheck() {
 		fmt.Println(down)
 	} else {
 		fmt.Println(up)
